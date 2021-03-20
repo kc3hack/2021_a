2d8ec14d5165399e39e224b81fffc03723d12a99
RosesAndViolets <qkrwn1249@gmail.com>
2021-03-20 17:52:33 +0900

    Fixed create_oauth2_url

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -1,18 +1,20 @@
 let user_signed_in = false;
-const CLIENT_ID = encodeURIComponent('328483009870-16hl1duvpbjsotq3565icbt32cdn2a7g.apps.googleusercontent.com');
-const RESPONSE_TYPE = encodeURIComponent('id_token');
-const REDIRECT_URI = encodeURIComponent('https://gnddapelcpfmifdcnpndfmcjdjcaadhg.chromiumapp.org');
-const STATE = encodeURIComponent('jfkls3n');
-const SCOPE = encodeURIComponent('openid');
-const PROMPT = encodeURIComponent('consent');
+var manifest = chrome.runtime.getManifest();
+var CLIENT_ID = encodeURIComponent('328483009870-16hl1duvpbjsotq3565icbt32cdn2a7g.apps.googleusercontent.com');
+// var RESPONSE_TYPE = encodeURIComponent('id_token');
+var REDIRECT_URI = encodeURIComponent('https://gnddapelcpfmifdcnpndfmcjdjcaadhg.chromiumapp.org');
+var STATE = encodeURIComponent('jfkls3n');
+var SCOPE = encodeURIComponent('openid');
+var PROMPT = encodeURIComponent('consent');
 
 function create_oauth2_url() {
   let nonce = encodeURIComponent(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
 
-  let url ='https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&redirect_uri=${REDIRECT_URI}&state=${STATE}&scope=${SCOPE}&prompt=${PROMPT}&nonce=${nonce';
+  let url = 'https://accounts.google.com/o/oauth2/v2/auth' +
+    '?client_id=' + CLIENT_ID + '&response_type=id_token' + '&redirect_uri=' + REDIRECT_URI + '&state=' + STATE + '&scope=' + SCOPE + '&prompt=' + PROMPT + '&nonce=' + nonce;
   
   
-  // console.log(url);
+  console.log(url);
 
   return url;
 }
